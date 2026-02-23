90905efb2c3d80fc1eeb5fbd3be3963b0b8a3ab7
jb9k62 <jb9k62@pm.me>
2026-02-23 06:38:08 +0200

    feat: add keymap for buffer snacks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>f/", function()
+  Snacks.picker.buffers({
+    hidden = true,
+    filter = {
+      filter = function(item)
+        return vim.bo[item.buf].buftype == "terminal"
+      end,
+    },
+  })
+end, { desc = "Terminal Buffers" })
